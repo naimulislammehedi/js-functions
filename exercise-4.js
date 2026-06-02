ce9a1271685f77c6d4c9f9085f828ad36a6b83f6
Mehedi <mehedi.interface@gmail.com>
2026-06-02 13:20:30 +0600

    Create a function that checks whether a number is positive or negative.

---
+++ exercise-4.js
@@ -0,0 +1,11 @@
+// Create a function that checks whether a number is positive or negative.
+function checkNumber(num) {
+    if (num >= 0) {
+        return "Positive"; 
+    } 
+
+    return "Negative"; 
+}
+
+console.log(checkNumber(10)); 
+console.log(checkNumber(-10)); 
