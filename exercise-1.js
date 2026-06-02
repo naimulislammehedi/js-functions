9300d04b024b0a47fb9dfa927b0ba02f8692b6a5
Mehedi <mehedi.interface@gmail.com>
2026-06-02 13:13:19 +0600

    What is the difference between Parameters and Arguments?

---
+++ exercise-1.js
@@ -0,0 +1,9 @@
+// What is the difference between Parameters and Arguments?
+function greet(name) { // Parameter
+    console.log(name);
+}
+
+greet("Mehedi"); // Argument
+
+// Parameter → Variable in function definition.
+// Argument → Actual value passed to the function.
