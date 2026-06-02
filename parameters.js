142fadde3ce6c6d4efbc78aa2d99e0e4939d6ea4
Mehedi <mehedi.interface@gmail.com>
2026-06-02 12:47:27 +0600

    JavaScript Parameters

---
+++ parameters.js
@@ -0,0 +1,14 @@
+// Function Parameters: Parameters are variables that recceive values when the function is called
+
+function greet(name) {
+    console.log("Hello " + name); 
+}
+
+greet("Mehedi"); 
+
+// Multiple parameters 
+function add(a, b) {
+    console.log(a + b); 
+}
+
+add(10, 20); 
