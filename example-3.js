d49eb6a407d8865109418e5ae4b464e954c350b1
Mehedi <mehedi.interface@gmail.com>
2026-06-02 13:11:55 +0600

    Real-Life Analogy

---
+++ example-3.js
@@ -0,0 +1,13 @@
+/*
+Real-Life Analogy
+==========================
+Think of a function like a coffee machine:
+* Coffee machine = Function
+* Coffee beans & water = Parameters
+* Making coffee = Processing
+* Coffee = Return value
+
+function makeCoffee(beans, water) {
+    return "Coffee Ready";
+}
+*/
